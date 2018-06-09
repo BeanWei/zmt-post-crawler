0f5acd2cb0e065762956c5a5802f418ac9b0bdd9
BeanWei <157318439@qq.com>
2018-06-09 18:16:11 +0800

    测试例子

--- test/test.go
+++ test/test.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"math/rand"
+	"sort"
 	"strings"
 	"time"
-)
 
-import (
 	"github.com/lxn/walk"
+
 	. "github.com/lxn/walk/declarative"
 )
 
@@ -24,16 +26,12 @@ type FooModel struct {
 	walk.SorterBase
 	sortColumn int
 	sortOrder  walk.SortOrder
-	evenBitmap *walk.Bitmap
-	oddIcon    *walk.Icon
 	items      []*Foo
 }
 
 func NewFooModel() *FooModel {
 	m := new(FooModel)
-	m.evenBitmap, _ = walk.NewBitmapFromFile("../img/open.png")
-	m.oddIcon, _ = walk.NewIconFromFile("../img/x.ico")
-	// m.ResetRows()
+	m.ResetRows()
 	return m
 }
 
@@ -77,57 +75,38 @@ func (m *FooModel) SetChecked(row int, checked bool) error {
 }
 
 // Called by the TableView to sort the model.
-// func (m *FooModel) Sort(col int, order walk.SortOrder) error {
-// 	m.sortColumn, m.sortOrder = col, order
+func (m *FooModel) Sort(col int, order walk.SortOrder) error {
+	m.sortColumn, m.sortOrder = col, order
 
-// 	sort.Stable(m)
+	sort.SliceStable(m.items, func(i, j int) bool {
+		a, b := m.items[i], m.items[j]
 
-// 	return m.SorterBase.Sort(col, order)
-// }
+		c := func(ls bool) bool {
+			if m.sortOrder == walk.SortAscending {
+				return ls
+			}
 
-func (m *FooModel) Len() int {
-	return len(m.items)
-}
+			return !ls
+		}
 
-// func (m *FooModel) Less(i, j int) bool {
-// 	a, b := m.items[i], m.items[j]
+		switch m.sortColumn {
+		case 0:
+			return c(a.Index < b.Index)
 
-// 	c := func(ls bool) bool {
-// 		if m.sortOrder == walk.SortAscending {
-// 			return ls
-// 		}
+		case 1:
+			return c(a.Bar < b.Bar)
 
-// 		return !ls
-// 	}
+		case 2:
+			return c(a.Baz < b.Baz)
 
-// 	switch m.sortColumn {
-// 	case 0:
-// 		return c(a.Index < b.Index)
+		case 3:
+			return c(a.Quux.Before(b.Quux))
+		}
 
-// 	case 1:
-// 		return c(a.Bar < b.Bar)
+		panic("unreachable")
+	})
 
-// 	case 2:
-// 		return c(a.Baz < b.Baz)
-
-// 	case 3:
-// 		return c(a.Quux.Before(b.Quux))
-// 	}
-
-// 	panic("unreachable")
-// }
-
-func (m *FooModel) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-
-// Called by the TableView to retrieve an item image.
-func (m *FooModel) Image(row int) interface{} {
-	if m.items[row].Index%2 == 0 {
-		return m.evenBitmap
-	}
-
-	return m.oddIcon
+	return m.SorterBase.Sort(col, order)
 }
 
 func (m *FooModel) ResetRows() {
@@ -154,7 +133,29 @@ func (m *FooModel) ResetRows() {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	boldFont, _ := walk.NewFont("Segoe UI", 9, walk.FontBold)
+	goodIcon, _ := walk.Resources.Icon("../img/check.ico")
+	badIcon, _ := walk.Resources.Icon("../img/stop.ico")
+
+	barBitmap, err := walk.NewBitmap(walk.Size{100, 1})
+	if err != nil {
+		panic(err)
+	}
+	defer barBitmap.Dispose()
+
+	canvas, err := walk.NewCanvasFromImage(barBitmap)
+	if err != nil {
+		panic(err)
+	}
+	defer barBitmap.Dispose()
+
+	canvas.GradientFillRectangle(walk.RGB(255, 0, 0), walk.RGB(0, 255, 0), walk.Horizontal, walk.Rectangle{0, 0, 100, 1})
+
+	canvas.Dispose()
+
 	model := NewFooModel()
+
+	var tv *walk.TableView
 
 	MainWindow{
 		Title:  "Walk TableView Example",
@@ -165,24 +166,77 @@ func main() {
 				Text:      "Reset Rows",
 				OnClicked: model.ResetRows,
 			},
-			HSplitter{
-				Name: "hSplitter",
-				Children: []Widget{
-					PushButton{
-						Text: "Do nothing",
-					},
-					TableView{
-						AlternatingRowBGColor: walk.RGB(255, 255, 224),
-						//CheckBoxes:            true,
-						//ColumnsOrderable:      true,
-						Columns: []TableViewColumn{
-							{Title: "#"},
-							{Title: "Bar"},
-							{Title: "Baz", Format: "%.2f", Alignment: AlignFar},
-							{Title: "Quux", Format: "2006-01-02 15:04:05", Width: 150},
-						},
-						Model: model,
-					},
+			PushButton{
+				Text: "Select first 5 even Rows",
+				OnClicked: func() {
+					tv.SetSelectedIndexes([]int{0, 2, 4, 6, 8})
+				},
+			},
+			PushButton{
+				Text: "查询",
+				OnClicked: func() {
+					viewindex := tv.SelectedIndexes()
+					log.Println(viewindex)
+					viewUrl := model.items[viewindex[0]].Bar
+					log.Println(viewUrl)
+				},
+			},
+			TableView{
+				AssignTo:              &tv,
+				AlternatingRowBGColor: walk.RGB(239, 239, 239),
+				CheckBoxes:            true,
+				ColumnsOrderable:      true,
+				MultiSelection:        false,
+				Columns: []TableViewColumn{
+					{Title: "#"},
+					{Title: "Bar"},
+					{Title: "Baz", Alignment: AlignFar},
+					{Title: "Quux", Format: "2006-01-02 15:04:05", Width: 150},
+				},
+				StyleCell: func(style *walk.CellStyle) {
+					item := model.items[style.Row()]
+
+					if item.checked {
+						if style.Row()%2 == 0 {
+							style.BackgroundColor = walk.RGB(159, 215, 255)
+						} else {
+							style.BackgroundColor = walk.RGB(143, 199, 239)
+						}
+					}
+
+					switch style.Col() {
+					case 1:
+						if canvas := style.Canvas(); canvas != nil {
+							bounds := style.Bounds()
+							bounds.X += 2
+							bounds.Y += 2
+							bounds.Width = int((float64(bounds.Width) - 4) / 5 * float64(len(item.Bar)))
+							bounds.Height -= 4
+							canvas.DrawBitmapPartWithOpacity(barBitmap, bounds, walk.Rectangle{0, 0, 100 / 5 * len(item.Bar), 1}, 127)
+
+							bounds.X += 4
+							bounds.Y += 2
+							canvas.DrawText(item.Bar, tv.Font(), 0, bounds, walk.TextLeft)
+						}
+
+					case 2:
+						if item.Baz >= 900.0 {
+							style.TextColor = walk.RGB(0, 191, 0)
+							style.Image = goodIcon
+						} else if item.Baz < 100.0 {
+							style.TextColor = walk.RGB(255, 0, 0)
+							style.Image = badIcon
+						}
+
+					case 3:
+						if item.Quux.After(time.Now().Add(-365 * 24 * time.Hour)) {
+							style.Font = boldFont
+						}
+					}
+				},
+				Model: model,
+				OnSelectedIndexesChanged: func() {
+					fmt.Printf("SelectedIndexes: %v\n", tv.SelectedIndexes())
 				},
 			},
 		},
